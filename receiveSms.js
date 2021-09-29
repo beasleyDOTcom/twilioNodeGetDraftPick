51b5501ebbd47b61f60076369c031f749a960e42
Beasley <beasley.com@gmail.com>
2021-09-29 15:27:12 -0700

    just formatted document before getting started on requirements

--- receiveSms.js
+++ receiveSms.js
@@ -9,23 +9,23 @@ const accountSid = process.env.SID;
 const authToken = process.env.authToken;
 
 const client = require('twilio')(accountSid, authToken);
-function handleSendText(number, phoneNumber){
+function handleSendText(number, phoneNumber) {
     client.messages.create({
         to: phoneNumber,
         from: process.env.twilioNumber,
         body: number.toString(),
     })
-    .then(
-        (message) => {
-            console.log('promise resolved');
-            console.log(message.sid)
-        }
-    );
+        .then(
+            (message) => {
+                console.log('promise resolved');
+                console.log(message.sid)
+            }
+        );
 }
 
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 let house = {};
@@ -35,26 +35,26 @@ let rooms = {};
 You should get rid of the password requirement and just use the hosts phone number --> the security of their phone is all that's needed (password would be visible in texts anyways)
 
 */
-async function tryCloseRoom(room, host){
-    async function shuffle(){
-                const getIndex = () => Math.floor(Math.random()*house[room].length);
-                const swap = async (arr, a, b) => {
-                    let temp = arr[a];
-                    arr[a] = arr[b];
-                    arr[b] = temp;
-                    return arr;
-                }
-                for(let i = 0; i < house[room].length; i++){
-                    await swap(house[room], i, getIndex());
-                    await swap(house[room], i, getIndex());
-                }
-            
+async function tryCloseRoom(room, host) {
+    async function shuffle() {
+        const getIndex = () => Math.floor(Math.random() * house[room].length);
+        const swap = async (arr, a, b) => {
+            let temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+            return arr;
+        }
+        for (let i = 0; i < house[room].length; i++) {
+            await swap(house[room], i, getIndex());
+            await swap(house[room], i, getIndex());
+        }
+
     }
-// validate request
-    if(rooms[room].host === host){
+    // validate request
+    if (rooms[room].host === host) {
         await shuffle();
         return 6;
-    } else{
+    } else {
         return 5;
     }
 }
@@ -68,34 +68,34 @@ async function tryHost(room, index, message, host) {
     //     index++;
     // }
     // if ().length > 0 && message[index] === ':') {
-        // we can continue
-        let command = '';
-        while (index < message.length) {
-            
-            // using toLowerCase in order to help prevent incedental errors from the phone's auto correct. 
-            command += message[index].toLowerCase();
-            index++;
-        }
-        console.log('this is command ', command)
-        // inspect command
-        if (command === 'open' || command === 'close') {
-            // command is good
-            if (command === 'open'){
-                if(house[room] === undefined){
-                    rooms[room] = { host };
-                    house[room] = [];
-                    return 1;
-                } else {
-                    return 7;
-                }
+    // we can continue
+    let command = '';
+    while (index < message.length) {
+
+        // using toLowerCase in order to help prevent incedental errors from the phone's auto correct. 
+        command += message[index].toLowerCase();
+        index++;
+    }
+    console.log('this is command ', command)
+    // inspect command
+    if (command === 'open' || command === 'close') {
+        // command is good
+        if (command === 'open') {
+            if (house[room] === undefined) {
+                rooms[room] = { host };
+                house[room] = [];
+                return 1;
+            } else {
+                return 7;
             }
-            else if (command === 'close') {
-                // shuffle array of phone numbers, then text each of them their index+1, then delete room from house.
-                return await tryCloseRoom(room, host);
-            }
-        } else {
-            return 0;
         }
+        else if (command === 'close') {
+            // shuffle array of phone numbers, then text each of them their index+1, then delete room from house.
+            return await tryCloseRoom(room, host);
+        }
+    } else {
+        return 0;
+    }
     // }
     // else {
     //     console.log('this is password: ', password);
@@ -108,10 +108,10 @@ async function tryHost(room, index, message, host) {
     //         return 3;
     //     }
     // }
-    
+
 }
-function tryParticipant(room){
-    if(house[room]!== undefined){
+function tryParticipant(room) {
+    if (house[room] !== undefined) {
         return true
     } else {
         return false;
@@ -128,10 +128,10 @@ app.post('/sms', async (req, res) => {
         room += message[index];
         index++;
     }
-    console.log('this is line36. ' + 'this is message: '+message +' and index: ' + index);
+    console.log('this is line36. ' + 'this is message: ' + message + ' and index: ' + index);
     if (message[index] === ':') {
-        let response = await tryHost(room, index+1, message, req.body.From);
-        switch(response){
+        let response = await tryHost(room, index + 1, message, req.body.From);
+        switch (response) {
             case 0:
                 twiml.message('Bad request. You must include a valid command. Example1 -> roomba:open  Example2 -> roomba:close');
                 break;
@@ -151,16 +151,16 @@ app.post('/sms', async (req, res) => {
                 twiml.message('Only the host can open or close a room. Please use the phone the room was opened with to close it.');
                 break;
             case 6:
- // shuffle array of phone numbers, then text each of them their index+1, then delete room from house.
+                // shuffle array of phone numbers, then text each of them their index+1, then delete room from house.
 
-                for(let i = 0; i < house[room].length; i++){
-                    handleSendText(i+1, house[room][i]);
+                for (let i = 0; i < house[room].length; i++) {
+                    handleSendText(i + 1, house[room][i]);
                 };
                 twiml.message('Working on sending everyone their random numbers.');
                 break;
             case 7:
                 twiml.message('Oops.. this room is taken! Please use a different name.')
-                
+
             default: console.log('ended up in the default case', response)
 
 
@@ -168,16 +168,16 @@ app.post('/sms', async (req, res) => {
     }
     else {
         let response = tryParticipant(room);
-        if(response){
+        if (response) {
             house[room].push(req.body.From);
             twiml.message('You have been added. Please wait for host to "close the room" (at which time you will receive your random number');
-        } 
+        }
         else {
-            twiml.message('There is not a room with this name: '+ req.body.Body + '. Please ensure you entered the room name correctly.');
+            twiml.message('There is not a room with this name: ' + req.body.Body + '. Please ensure you entered the room name correctly.');
         }
     }
 
-    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
     res.end(
         twiml.toString()
     );
